1cd6340da4b4d11bdea04f941bf58297e9ff5f90
shige <shige@asiainfo.com>
2022-11-01 19:48:04 +0800

    feat: vite配置改用函数&引入配置文件

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,24 @@
-import { defineConfig } from 'vite'
+import { ConfigEnv, defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  // 基础地址
-  base: '/shige',
-  plugins: [vue()]
+// export default defineConfig({
+//   // 基础地址
+//   base: '/shige',
+//   plugins: [vue()]
+// })
+
+export default defineConfig((mode) => {
+  // 拼接当前环境文件名
+  const envFileName: string = '.env'
+  const curEnvFileName: string = `${envFileName}.${mode.mode}`;
+  if (mode.mode === 'development') {
+    console.log('开发环境')
+  }
+  else if (mode.mode === 'production') {
+    console.log('生产环境')
+  }
+  return {
+    plugins: [vue()]
+  }
 })
